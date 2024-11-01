af520fa37e2ead60af53925a864d58d4ee9d3a79
Taylor Ji <sji24@my.bcit.ca>
2024-10-31 23:23:09 -0700

    completed testing all variables that will be stored into the sql server

--- main.py
+++ main.py
@@ -11,5 +11,21 @@ cursor = con.cursor()
 
 while 1==1:
     cpu_usage = psutil.cpu_percent()
+    # extracting the percent value from the array
+    memory_usage = psutil.virtual_memory()[2]
+
+    cpu_interrups = psutil.cpu_stats()[1]
+    cpu_calls = psutil.cpu_stats()[3]
+
+    memory_used = psutil.virtual_memory()[3]
+    memory_free = psutil.virtual_memory()[4]
+
+    bytes_sent = psutil.net_io_counters()[0]
+    bytes_received = psutil.net_io_counters()[1]
+
+    disk_usage = psutil.disk_usage('/')[3]
+
     print(cpu_usage)
+    print(memory_usage)
+    print(bytes_sent)
     time.sleep(1)
